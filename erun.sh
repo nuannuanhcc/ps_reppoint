674ffc70b234ec85a5a73433c89e034e248be18e
hcc <hcc@hust.edu.cn>
2020-07-03 02:21:36 +1000

    7_2_2_test

--- erun.sh
+++ erun.sh
@@ -1,5 +1,5 @@
 # python ./mmdetection/tools/test.py ./configs/reppoints_moment_r50_fpn_1x.py work_dirs/reppoints_moment_r50_fpn_1x_6_15_3/epoch_12.pth --out results.pkl --eval bbox
 
- python ./mmdetection/tools/test.py ./configs/reppoints_moment_r50_fpn_2x.py work_dirs/reppoints_moment_r50_fpn_2x_7_2_3/epoch_24.pth --out results.pkl --eval bbox
+ python ./mmdetection/tools/test.py ./configs/reppoints_moment_r50_fpn_2x.py work_dirs/reppoints_moment_r50_fpn_2x_7_2_2/epoch_24.pth --out results.pkl --eval bbox
 
 # python ./mmdetection/tools/test.py ./mmdetection/configs/retinanet_r50_fpn_1x.py work_dirs/retinanet_r50_fpn_1x_6_15_0/epoch_12.pth --out results.pkl --eval bbox
