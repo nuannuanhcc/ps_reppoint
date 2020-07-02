ddd890023d99a54306068927ee56e97615fb156f
hcc <hcc@hust.edu.cn>
2020-07-02 22:53:55 +1000

    Auto stash before merge of "master" and "origin/master"

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python ./mmdetection/tools/train.py ./configs/test.py
-#python ./mmdetection/tools/train.py ./configs/reppoints_moment_r50_fpn_1x.py
+#python ./mmdetection/tools/train.py ./configs/test.py
+python ./mmdetection/tools/train.py ./configs/reppoints_moment_r50_fpn_1x.py
 #python ./mmdetection/tools/train.py ./configs/reppoints_moment_r50_fpn_2x.py
 #python ./mmdetection/tools/train.py ./mmdetection/configs/retinanet_r50_fpn_1x.py
